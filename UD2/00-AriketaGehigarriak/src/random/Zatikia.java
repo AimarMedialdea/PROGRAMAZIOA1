09778136f062e6b3cca9f43276fce4082af7bc8e
Aimar Medialdea <medialdea.aimar@uni.eus>
2022-11-24 10:01:15 +0100

    24/11/22

--- UD2/00-AriketaGehigarriak/src/random/Zatikia.java
+++ UD2/00-AriketaGehigarriak/src/random/Zatikia.java
@@ -17,11 +17,65 @@ public class Zatikia {
         return izendatzailea;
     }
 
-    public void setZenbakitzailea(int zenba){
-        zenbakitzailea = zenba;
+    public void setZenbakitzailea(int newZenbakitzailea){
+        zenbakitzailea = newZenbakitzailea;
     }
 
-    public void setZenbakitzailea(int izenda){
-        izendatzailea = izenda;
+    public void setIzendatzailea(int newIzenda){
+        izendatzailea = newIzenda;
     }
+
+    public String toString(){
+        return "s";
+    }
+
+    public static Zatikia biderkatu(Zatikia zat1, Zatikia zat2){
+        int zenb, izend;
+
+        zenb = zat1.zenbakitzailea * zat2.zenbakitzailea;
+        izend = zat1.izendatzailea * zat2.izendatzailea;
+
+        Zatikia zat3 = new Zatikia(zenb, izend);
+        return zat3;
+    } 
+
+    public static Zatikia batu(Zatikia zat1, Zatikia zat2){
+        int zenbaki;
+        int izend;
+
+        izend = zat1.getIzendatzailea() * zat2.getIzendatzailea();
+        zenbaki = zat1.getIzendatzailea() * zat2.getZenbakitzailea() + zat1.getZenbakitzailea() * zat2.getIzendatzailea();
+
+        Zatikia batuketa = new Zatikia(zenbaki, izend);
+        return batuketa;
+    }
+
+    public void batu(Zatikia besteZatBat){
+        int zenbaki;
+        int izend;
+
+        zenbaki = this.getZenbakitzailea() * this.getZenbakitzailea();
+        izend = this.getIzendatzailea() * besteZatBat.getZenbakitzailea() + this.getZenbakitzailea() * besteZatBat.getIzendatzailea();
+
+        this.setIzendatzailea(izend);
+        this.setZenbakitzailea(zenbaki);
+    }
+
+    public double hamartarBaliokidea(){
+        double baliokidea;
+
+        baliokidea = zenbakitzailea / izendatzailea;
+        return baliokidea;
+    }
+
+    public boolean isBiggerThan(Zatikia besteZatikiBat){
+        boolean haundiagoa = false;
+
+        if(this.hamartarBaliokidea() > besteZatikiBat.hamartarBaliokidea()){
+            haundiagoa = true;
+        }
+        return haundiagoa;
+
+    }
+
 }

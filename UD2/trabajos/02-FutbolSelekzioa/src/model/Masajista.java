80a5680a288c4c165617b9cdb6600eeb427f65ff
Aimar Medialdea <medialdea.aimar@uni.eus>
2023-01-23 14:22:46 +0100

    furbo

---
+++ UD2/trabajos/02-FutbolSelekzioa/src/model/Masajista.java
@@ -0,0 +1,33 @@
+package model;
+public class Masajista {
+    
+    private String titulacion;
+    private int aniosExperiencia;
+    
+    public Masajista(String titulacion, int aniosExperiencia) {
+        this.titulacion = titulacion;
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    public String getTitulacion() {
+        return titulacion;
+    }
+
+    public void setTitulacion(String titulacion) {
+        this.titulacion = titulacion;
+    }
+
+    public int getAniosExperiencia() {
+        return aniosExperiencia;
+    }
+
+    public void setAniosExperiencia(int aniosExperiencia) {
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    public void darMasaje(){
+
+    }
+
+    
+}
